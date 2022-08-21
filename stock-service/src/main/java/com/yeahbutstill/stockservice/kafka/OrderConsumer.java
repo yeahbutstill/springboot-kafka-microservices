523e1d525f5c21c86105f5962b089ae1befa1268
yeahbutstill <mayadani28@gmail.com>
2022-08-21 20:46:15 +0700

    Configure and Create Kafka Consumer in StockService

--- stock-service/src/main/java/com/yeahbutstill/stockservice/kafka/OrderConsumer.java
+++ stock-service/src/main/java/com/yeahbutstill/stockservice/kafka/OrderConsumer.java
@@ -1,2 +1,24 @@
-package com.yeahbutstill.stockservice.kafka;public class OrderConsumer {
+package com.yeahbutstill.stockservice.kafka;
+
+import com.yeahbutstill.basedomains.dto.OrderEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class OrderConsumer {
+
+    @KafkaListener(
+            topics = "${spring.kafka.topic.name}",
+            groupId = "${spring.kafka.consumer.group-id}"
+    )
+    public void consume(OrderEvent orderEvent) {
+
+        log.info("Order event received in stock service => {}", orderEvent);
+
+        // save the order event into the database
+
+    }
+
 }
